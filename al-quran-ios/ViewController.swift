447775c47bb4570c9c42a73f9a04423b74c36f91
Iqbal Ansyori <ansyori.iqbal@gmail.com>
2019-11-19 21:20:11 +0700

    Remove storyboard

--- al-quran-ios/ViewController.swift
+++ al-quran-ios/ViewController.swift
@@ -2,19 +2,17 @@
 //  ViewController.swift
 //  al-quran-ios
 //
-//  Created by Admin on 19/11/19.
+//  Created by Iqbal Ansyori on 19/11/19.
 //  Copyright © 2019 Admin. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .green
     }
-
-
 }
-
